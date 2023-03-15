d75f2ab10107abd7ab274c1974f7b2287f73f34e
Tejasp2003 <tejasp200320@gmail.com>
2023-03-15 21:49:02 +0530

    Topological sort

--- GFG/Topological Sort/Solution.java
+++ GFG/Topological Sort/Solution.java
@@ -1,3 +1,10 @@
+//what is topological sort?
+//Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge uv, vertex u comes before v in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+
+//For example, a topological sorting of the following graph is “5 4 2 3 1 0”.
+
+
+
 import java.util.*;
 
 class Solution {

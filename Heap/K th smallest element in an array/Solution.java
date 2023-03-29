83ffe009c0c79d60659486cf41ccce32d1c7c127
Tejasp2003 <tejasp200320@gmail.com>
2023-03-29 23:24:38 +0530

    k th smallest element in an array

---
+++ Heap/K th smallest element in an array/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution{
+    public static int kthSmallest(int[] arr,int k) 
+    { 
+        int size = arr.length;
+        PriorityQueue<Integer> maxHeap= new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int i =0; i<size; i++){
+            maxHeap.add(arr[i]);
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+        return maxHeap.peek();
+    } 
+    public static void main(String[] args) {
+        
+        int [] arr= {7, 10, 4, 3, 20, 15};
+        int k = 3;
+        System.out.println(kthSmallest(arr, k));
+
+    }
+
+}
